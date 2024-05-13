bb84c39cc0e95c156746f2248c40b4a8b0318cd5
Onixdrew <andrewmosquera1811@gmail.com>
2024-05-13 12:34:56 -0500

    Updated

--- GestioPeliculas/urls.py
+++ GestioPeliculas/urls.py
@@ -21,6 +21,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.inicio),
+    
     # formularo para agregar genero
     path('vistaAgregarGenero/', views.vistaAgregarGenero),
     path('agregarGenero/', views.agregarGenero),
